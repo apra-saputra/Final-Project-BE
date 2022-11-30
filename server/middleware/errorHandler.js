380b055e42955b71141db36e0556a4af55a4aeb4
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-11-30 16:27:32 +0800

    feat(errorHandler): function error handling

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,31 @@
+const errorHandler = (err, req, res, next) =>{
+  let code = 500
+  let message = "internal server error"
+
+  if(err.name === 'SequelizeValidationError' || err.name === 'SequelizeUniqueConstraintError'){
+    let errMsg = err.errors.map(el => el.message)
+    code = 400
+    message = errMsg[0]
+  } else if(err.name === 'invalid_login'){
+    code = 401
+    message = "error invalid email or password"
+  } else if(err.name === "project_not_found"){
+    code = 404
+    message = "project not found"
+  } else if(err.name === "tag_not_found"){
+    code = 404
+    message = "tag not found"
+  } else if(err.name === "forbidden"){
+    code = 403
+    message = "your not authorize"
+  } else if(err.name === "invalid_token" ||err.name === "JsonWebTokenError"){
+    code = 401
+    message = "error authentication - invalid token"
+  } else if(err.name === "duplicate_favorite"){
+    code = 400
+    message = "project already on your favorite"
+  }
+  res.status(code).json({message})
+}
+
+module.exports = errorHandler
