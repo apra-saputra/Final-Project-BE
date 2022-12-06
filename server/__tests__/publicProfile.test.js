9f937a4b72b68aeb71a05498f0647fb949372503
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-12-06 16:52:37 +0800

    test: profile user endpoint test

---
+++ server/__tests__/publicProfile.test.js
@@ -0,0 +1,64 @@
+const request = require('supertest')
+const app = require('../app')
+const { sequelize, User } = require('../models')
+const { queryInterface } = sequelize
+const { hashPassword } = require('../helpers/bcrypt')
+const { encodeToken } = require('../helpers/jwt')
+
+const dateToday = new Date()
+let validToken = encodeToken({ id : 1, role: 'Users' })
+let inValidToken = '123455cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjY3NTQ3ODg5fQ.RtrucOpSAthM1qxqo6MLXb2eI3_r8w6u_-KZ9CXoJ5'
+
+beforeAll(()=>{
+  return queryInterface.bulkDelete('Users', null, {
+    truncate: true, 
+    cascade:true,
+    restartIdentity:true
+  })
+    .then((result)=>{
+      return queryInterface.bulkInsert('Users', [{
+        "fullname": 'Testing',
+        "username": 'testing',
+        "email": 'registered@mail.com',
+        "role": "Users",
+        "password": hashPassword('123123123'),
+        "createdAt": dateToday,
+        "updatedAt": dateToday
+      }], {})
+    })
+  })
+
+afterAll(()=>{
+  return queryInterface.bulkDelete('Users', null, {
+    truncate: true,
+    cascade:true,
+    restartIdentity:true
+  })
+})
+
+describe('GET /public/profile - Profile admin', () => {
+  test('GET /public/profile - success', () => {
+    return request(app)
+      .get('/public/profile')
+      .set('access_token', validToken)
+      .then(res=>{
+        expect(res.status).toBe(200)
+      })
+  });
+  test('GET /public/profile - fail', () => {
+    return request(app)
+      .get('/public/profile')
+      .then(res=>{
+        expect(res.status).toBe(401)
+      })
+  });
+  test('GET /public/profile - fail', () => {
+    return request(app)
+      .get('/public/profile')
+      .set('access_token', inValidToken)
+      .then(res=>{
+        expect(res.status).toBe(401)
+        expect(res.body).toHaveProperty('message', 'error authentication - invalid token');
+      })
+  });
+});
