d3fa27203f74dac00782599a8cc26c6e4fd478dc
Jonathan <jrs01210@gmail.com>
2022-12-06 12:51:43 +0700

    added Tag Controller and route in public for fetching tags

--- server/routes/public.js
+++ server/routes/public.js
@@ -3,15 +3,17 @@ const CommentController = require('../controllers/Comment');
 const router = express.Router();
 const Users = require('../controllers/User');
 const Projects = require('../controllers/Projects');
+const Tags = require('../controllers/Tag');
 const authentication = require('../middleware/authentication');
 const Authorization = require('../middleware/authorization');
 const favorite = require('./favorite')
 const project = require('./project');
-const report = require('./report')
+const report = require('./report');
 
 router.post('/login', Users.Login);
 router.post('/register', Users.Register);
 router.get('/projects', Projects.Read);
+router.get('/tags', Tags.readTags);
 router.get('/projects/:id', Projects.ReadDetail);
 router.use(authentication)
 router.get('/profile', Users.getProfile);
