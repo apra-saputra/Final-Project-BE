143edff8ffce388e1b70fb93379c6ee533737021
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-12-11 21:26:18 +0700

    feat: -confiq socket io for admin - add new commit socket io for report

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const server = require('http').createServer(app);
 app.use(cors())
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","http://localhost:3001"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
   },
